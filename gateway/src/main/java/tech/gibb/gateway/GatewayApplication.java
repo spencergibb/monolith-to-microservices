7aa07a4c49eb3bf824e62fc3b3891142847f0781
Spencer Gibb <spencer@gibb.us>
2018-05-01 22:49:12 -0400

    Front existing monolith with gateway

--- gateway/src/main/java/tech/gibb/gateway/GatewayApplication.java
+++ gateway/src/main/java/tech/gibb/gateway/GatewayApplication.java
@@ -2,9 +2,20 @@ package tech.gibb.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p.path("/**")
+						.uri("http://localhost:8081"))
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
