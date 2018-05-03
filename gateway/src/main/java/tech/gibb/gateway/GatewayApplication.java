b036ea30b4d58d1dea9bfe610013161331c5bb79
Spencer Gibb <spencer@gibb.us>
2018-05-02 21:47:31 -0400

    Adds circuit breaker to old fortune service

--- gateway/src/main/java/tech/gibb/gateway/GatewayApplication.java
+++ gateway/src/main/java/tech/gibb/gateway/GatewayApplication.java
@@ -6,13 +6,19 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class GatewayApplication {
 
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
+				.route("fortune", p -> p.path("/service/randomfortune")
+						.filters(f -> f.hystrix(c -> c.setFallbackUri("forward:/defaultfortune")))
+						.uri("http://localhost:8081"))
 				.route("hello_rewrite", p -> p.path("/service/hello/**")
 						.filters(f -> f.filter((exchange, chain) -> {
 							String name = exchange.getRequest().getQueryParams().getFirst("name");
@@ -31,6 +37,11 @@ public class GatewayApplication {
 				.route("monolith", p -> p.path("/**")
 						.uri("http://localhost:8081"))
 				.build();
+	}
+
+	@RequestMapping("/defaultfortune")
+	public String defaultFortune() {
+		return "When you feel sad: start dot spring dot io.";
 	}
 
 	public static void main(String[] args) {
