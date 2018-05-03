74d9c610b51052d050d85365c4acde7dd74f4e4a
Spencer Gibb <spencer@gibb.us>
2018-05-02 21:32:12 -0400

    Moves hello service to standalone app.

--- gateway/src/main/java/tech/gibb/gateway/GatewayApplication.java
+++ gateway/src/main/java/tech/gibb/gateway/GatewayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -12,6 +13,16 @@ public class GatewayApplication {
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
+				.route("hello_rewrite", p -> p.path("/service/hello/**")
+						.filters(f -> f.filter((exchange, chain) -> {
+							String name = exchange.getRequest().getQueryParams().getFirst("name");
+							String path = "/hello/"+name;
+							ServerHttpRequest request = exchange.getRequest().mutate()
+									.path(path)
+									.build();
+							return chain.filter(exchange.mutate().request(request).build());
+						}))
+						.uri("lb://hello"))
 				.route("index", p -> p.path("/")
 						.filters(f -> f.setPath("/index.html"))
 						.uri("lb://ui"))
--- hello/src/main/java/tech/gibb/hello/HelloApplication.java
+++ hello/src/main/java/tech/gibb/hello/HelloApplication.java
@@ -2,9 +2,18 @@ package tech.gibb.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class HelloApplication {
+
+	@RequestMapping("/hello/{name}")
+	public String hello(@PathVariable String name) {
+		return "HELLO " + name;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
